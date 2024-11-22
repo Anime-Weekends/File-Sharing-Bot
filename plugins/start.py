1bcf233d036ff5b7b02e740c2ae68e8cbe5c19df
rohit3607 <rohitplayer87089@gmail.com>
2024-11-23 00:25:42 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,6 +6,7 @@ from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from bot import Bot
+from config import *
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, FILE_AUTO_DELETE
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
@@ -13,6 +14,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 madflixofficials = FILE_AUTO_DELETE
 jishudeveloper = madflixofficials
 file_auto_delete = humanize.naturaldelta(jishudeveloper)
+
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
@@ -213,4 +215,4 @@ async def send_text(client: Bot, message: Message):
 <b>Successful :</b> <code>{successful}</code>
 <b>Blocked Users :</b> <code>{blocked}</code>
 <b>Deleted Accounts :</b> <code>{deleted}</code>
-<b>Unsuccessful :</b
+<b>Unsuccessful :</b>
